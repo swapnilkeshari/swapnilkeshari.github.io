50245b2c8f2162638272a3d4650943789c6f7cef
swapnilkeshari <swk25@pitt.edu>
2024-12-21 18:15:55 -0500

    updated poems

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,17 @@
-import js from '@eslint/js'
 import { FlatCompat } from '@eslint/eslintrc'
 
 const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
-  recommendedConfig: js.configs.recommended,
 })
 
 const eslintConfig = [
   ...compat.config({
-    extends: ['eslint:recommended', 'next'],
+    extends: ['"next/core-web-vitals"'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
   }),
 ]
 
